5f57dea0a88b2839c2bb5ce54cac7651d63fbbc6
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-06-01 02:45:06 +0100

    Feat: add delete task functionality

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -55,6 +55,17 @@ export const editTask = async (req, res) => {
     }
 }
 
-export const deleteTask = (req, res) => {
-    console.log("delete task")
+export const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params
+        const checkExitence = await Task.findById(id)
+        if(!checkExitence){
+            return res.status(404).json({mesage: `task with id ${id} not found`})
+        }
+        await Task.findByIdAndDelete(id)
+        res.status(200).json({message: "task deleted successfully"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "error deleting post"})
+    }
 }
