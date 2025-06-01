1672ae20d4306520a98f2b4138bc431c3d573add
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-06-01 02:21:35 +0100

    Feat: Add edit task functionality in the backend

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -17,8 +17,7 @@ export const createTask = async (req, res) => {
             return res.status(400).json("Task cannot be empty")
         }
         const newTask = new Task({
-            text: task.trim(),
-            completed: false
+            text: task.trim()
         })
         await newTask.save()
         res.status(201).json(newTask)
@@ -30,12 +29,26 @@ export const createTask = async (req, res) => {
 
 export const editTask = async (req, res) => {
     try {
-        const {id} = req.params
-        const checkExitence = Task.findById(id)
+        const { id } = req.params
+        const { task, dueDate } = req.body
+        const checkExitence = await Task.findById(id) 
         if(!checkExitence){
             return res.status(404).json({message: `No task with id ${id}`})
         }
-        res.status(200).json(id)
+        console.log(req.body);
+        
+        if (!task || task.trim() === '') {
+            return res.status(400).json({message: "task field cannot be empty"})
+        }
+        const editedTask = {
+            text: task,
+            completed: false
+        }
+        if (typeof dueDate !== 'undefined' && dueDate !== '') {
+            editedTask.dueDate = dueDate
+        }
+        const updatedTask = await Task.findByIdAndUpdate(id, editedTask, {new: true})
+        res.status(200).json(updatedTask)
     } catch (error) {
         console.log(error)
         res.status(500).json({message: "error editing task"})
