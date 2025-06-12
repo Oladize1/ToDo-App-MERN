6a9ca655587489020f3bd5eb23d74f5e73022b66
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-06-12 22:36:15 +0100

    feat: start implementation on the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,9 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
-import TaskInput from "./components/TaskInput"
-import LeftSide from "./components/LeftSide"
-import RightSide from "./components/RightSide"
-const BASEURL = 'https://todo-app-mern-dzti.onrender.com'
+import React from "react"
+import HomePage from "./Pages/HomePage"
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [filter, setFilter] = useState({
-      all: true,
-      completed: false,
-      unCompleted: false,
-      sortByDate: false
-  })
-  const [tasks, setTasks] = useState([])
-
-  async function getTasks() {
-    setLoading(true)
-    try {
-      const getAllTask = await axios.get(`${BASEURL}/api/tasks`)
-      setTasks(getAllTask.data)
-      setLoading(false)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-    useEffect(() => {
-      getTasks()    
-    },[])
-
   return (
     <>
-      <header className="text-center mt-7">
-        <p className="text-4xl font-bold">Donezo</p>
-        <p className="text-sm">Stay Organized and productive</p>
-      </header>
-      <main className="m-10">
-          <TaskInput getTasks={getTasks} filter={filter}/>
-          <section className="flex gap-4 w-full flex-wrap mt-7 md:flex-nowrap">
-            <LeftSide tasks={tasks} getTasks={getTasks} filter={filter} setfilter={setFilter} />
-            <RightSide tasks={tasks}/>
-          </section>
-      </main>
+    <HomePage/>
     </>
   )
 }
---
+++ client/src/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='flex justify-center items-center'>
+        <p className="text-4xl font-bold">Donezo</p>
+        <p className="text-sm">Stay Organized and productive</p>
+    </div>
+  )
+}
+
+export default LandingPage
