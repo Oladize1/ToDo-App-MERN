c40f0fd5a242758ec5cf0f78a8ae68ee51d64787
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-06-14 00:44:07 +0100

    Feat: Done with login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,40 @@
-import React from "react"
-import {Routes, Route} from 'react-router-dom'
+import React, {useEffect} from "react"
+import {Routes, Route, useNavigate} from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import HomePage from "./Pages/HomePage"
 import LandingPage from "./Pages/LandingPage"
 import RegisterPage from "./Pages/registerPage"
 import LoginPage from "./Pages/LoginPage"
 import ProtectedRoute from "./components/ProtectedRoute"
+import axios from "axios"
+const BASEURL = 'http://localhost:8080/api/auth'
 function App() {
+  const navigate = useNavigate()
+  useEffect(()=>{
+    verifyUser()
+  },[])
+
+  async function verifyUser(){
+    const token = localStorage.getItem('token')
+    if (!token) {
+      navigate('/login')
+      return
+    }
+    try {
+      const res = await axios.get(`${BASEURL}/verify-token`, {
+        headers: {
+          'Authorization' : `Bearer ${token}`
+        }
+      })
+      if (res.data.status === 200) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+    }
+  }
   return (
     <>
     <Routes>
--- client/src/Pages/registerPage.jsx
+++ client/src/Pages/registerPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'; 
 
+import Spinner from '../components/Spinner';
+
+const BASEURL = 'http://localhost:8080/api/auth/'
 const RegisterPage = () => {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleRegister = async(e) => {
+    setLoading(true)
+    e.preventDefault()
+    try {
+      if (username.trim() === '' || email.trim() === '' || password === '') {
+        alert('invalid field')
+        setLoading(false)
+        return
+      }
+      const res = await axios.post(`${BASEURL}/register`, {email, username, password})
+      setLoading(false)
+      if (res.status === 200) {
+        localStorage.setItem('token', res.data.token)
+        localStorage.setItem('user', res.data.username)
+      }
+      setUsername('')
+      setEmail('')
+      setPassword('')
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+      alert(error.response.data.message)
+      setLoading(false)
+    }
+  }
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 px-4">
+      {loading && <Spinner/>}
       <div className="w-full max-w-md bg-white rounded-3xl shadow-lg p-8 sm:p-10">
         <h2 className="text-center text-3xl font-bold text-gray-900">
           Create your account
@@ -12,7 +49,7 @@ const RegisterPage = () => {
             Stay Organized and Productive
         </p>
 
-        <form className="mt-8 space-y-6">
+        <form className="mt-8 space-y-6" onSubmit={handleRegister}>
           {/* Username */}
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -27,6 +64,8 @@ const RegisterPage = () => {
               maxLength="30"
               title="Only letters, numbers or dash"
               className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <p className="mt-1 text-xs text-gray-500">
               3–30 characters, letters/numbers/dashes only
@@ -43,6 +82,8 @@ const RegisterPage = () => {
               placeholder="mail@site.com"
               required
               className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <p className="mt-1 text-xs text-gray-500">We'll never share your email.</p>
           </div>
@@ -60,6 +101,8 @@ const RegisterPage = () => {
               pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
               title="Must contain 8+ characters including number, lowercase, and uppercase"
               className="w-full px-4 py-2 border rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <p className="mt-1 text-xs text-gray-500">
               Must be 8+ characters with number, lowercase, and uppercase.
